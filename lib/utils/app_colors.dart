721f10ade3e1b70a4727193e6f8b54d553151791
Asem <asemabdo2131999@gmail.com>
2025-07-11 19:40:08 +0300

    Complete navigation bar at home screen. Build tabs widget for home screen to start with it.

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -4,4 +4,7 @@ abstract final class AppColors {
   static const gray = Color(0xff202020);
   static const gold = Color(0xffE2BE7F);
   static const whiteGray = Color(0xff707070);
+  static const white = Color(0xffFFFFFF);
+  static const black = Color(0xff202020);
+  static const blackWithOpacity60 = Color(0x99202020);
 }
--- lib/utils/screens/home_screen/home_screen.dart
+++ lib/utils/screens/home_screen/home_screen.dart
@@ -1,42 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:islami/utils/app_assets.dart';
+import 'package:islami/utils/app_colors.dart';
+import 'package:islami/utils/screens/home_screen/tabs/ahadith_tab.dart';
+import 'package:islami/utils/screens/home_screen/tabs/quran_tab.dart';
+import 'package:islami/utils/screens/home_screen/tabs/radio_tab.dart';
+import 'package:islami/utils/screens/home_screen/tabs/sebha_tab.dart';
+import 'package:islami/utils/screens/home_screen/tabs/time_tab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const routeName = "/homeScreen";
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+  List<Widget> tabs = [
+    QuranTab(),
+    AhadithTab(),
+    SebhaTab(),
+    RadioTab(),
+    TimeTab(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: buildBottomNavagationBar(),
-      body: Container(),
+      bottomNavigationBar: buildBottomNavigationBar(),
+      body: tabs[selectedIndex],
     );
   }
 
-  Widget buildBottomNavagationBar() {
+  Widget buildBottomNavigationBar() {
     return BottomNavigationBar(
+      selectedItemColor: AppColors.white,
+      unselectedItemColor: AppColors.black,
+      onTap: (index) {
+        selectedIndex = index;
+        setState(() {});
+      },
+      currentIndex: selectedIndex,
       items: [
         BottomNavigationBarItem(
-          icon: ImageIcon(AssetImage(AppAssetes.ic_quran)),
+          icon: buildBottomNavBarIcon(
+            AssetImage(AppAssetes.ic_quran),
+            selectedIndex == 0,
+          ),
           label: 'Quran',
         ),
         BottomNavigationBarItem(
-          icon: ImageIcon(AssetImage(AppAssetes.ic_hadith)),
+          icon: buildBottomNavBarIcon(
+            AssetImage(AppAssetes.ic_hadith),
+            selectedIndex == 1,
+          ),
           label: 'Hadith',
         ),
         BottomNavigationBarItem(
-          icon: ImageIcon(AssetImage(AppAssetes.ic_sebha)),
+          icon: buildBottomNavBarIcon(
+            AssetImage(AppAssetes.ic_sebha),
+            selectedIndex == 2,
+          ),
           label: 'Sebha',
         ),
         BottomNavigationBarItem(
-          icon: ImageIcon(AssetImage(AppAssetes.ic_radio)),
+          icon: buildBottomNavBarIcon(
+            AssetImage(AppAssetes.ic_radio),
+            selectedIndex == 3,
+          ),
           label: 'Radio',
         ),
         BottomNavigationBarItem(
-          icon: ImageIcon(AssetImage(AppAssetes.ic_time)),
+          icon: buildBottomNavBarIcon(
+            AssetImage(AppAssetes.ic_time),
+            selectedIndex == 4,
+          ),
           label: 'Time',
         ),
       ],
     );
   }
+}
+
+Widget buildBottomNavBarIcon(AssetImage assetsImage, bool isSelected) {
+  return Container(
+    decoration: BoxDecoration(
+      color: isSelected ? AppColors.blackWithOpacity60 : Colors.transparent,
+      borderRadius: BorderRadius.circular(66),
+    ),
+    padding: EdgeInsets.symmetric(vertical: 6, horizontal: 20),
+    child: ImageIcon(assetsImage),
+  );
 }
